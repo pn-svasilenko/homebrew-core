2da572468f73c38939b52fc6c4d2704cd5d8894e
Sergey Vasilenko <svasilenko@peoplenetonline.com>
2018-05-18 11:16:10 +0300

    Update rancher-cli.rb

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -2,7 +2,7 @@ class RancherCli < Formula
   desc "The Rancher CLI is a unified tool to manage your Rancher server"
   homepage "https://github.com/rancher/cli"
   url "https://github.com/rancher/cli/archive/v0.6.8.tar.gz"
-  sha256 "3c7281efb5dced04588dea2b943ebbe0cb5b1621eeea97f6c477713d79c7c1bc"
+  sha256 "2a3b6b83d9e39f9ed090f342c40a1213c97713f87d8dab06249e169e7a4b7fc0"
 
   bottle do
     cellar :any_skip_relocation
